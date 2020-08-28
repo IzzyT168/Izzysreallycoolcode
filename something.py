c8c5f4038e28b3fa0f32bb39b5b4df996b6ef86a
Izzy <issie_99@hotmail.com>
2020-08-29 03:40:21 +1200

    Z bot update

--- something.py
+++ something.py
@@ -1,5 +1,5 @@
 import discord 
-
+import youtube.d1
 import pw
 TOKEN = pw.TOKEN
 
@@ -14,9 +14,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content=='$p' :
-        await message.channel.send('poggies')
-    elif message.content == '$q' :
-        await message.channel.send('Hey queen, I saw you post you hate men on your profile and I just want to say I totally agree, even though I am one (ugh). I do want to say that I am one of the good ones though, but I thoroughly condemn the rest of my gender. Btw your tits are pretty dope lol I hadn’t noticed until now')
+        await message.channel.send["Hello" , "Poggies" , "Good morning"]
     elif message.content == '$h' :
         await message.channel.send('I drink tons of horse cum to kill the demon in my stomach who constantly tells me not to drink horse cum. Do you know how hard it is to acquire a gallon of horse cum a day and also hold a job and a family? No you dont. Neither do I because I dont actually have a job or a family which makes acquiring horse cum a little harder and easier at the same time.')
 
